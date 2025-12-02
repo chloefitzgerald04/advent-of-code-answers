b45eed33815c2586d3190db138b7c9b9b9c4a601
C Fitzgerald <84927940+chloefitzgerald04@users.noreply.github.com>
2025-12-02 13:48:20 +0000

    Create Part-2.ps1

---
+++ Day 2/Part-2.ps1
@@ -0,0 +1,16 @@
+$ids = @((get-content -path "C:\Temp\aoc2.csv" -delimiter ",").replace(",",""))
+$invalididsum = 0
+
+
+foreach($id in $ids){
+    for($num = ([int64]$id.split("-")[0]); $num -le ([int64]$id.split("-")[1]); $num++){
+        $numstring = "$num"
+        #Regex catches any repeating string
+        if ($numstring -match '^(.+)\1+$') {
+            $repeating = $matches[1]
+            $global:invalididsum += $num
+        }
+    }
+
+} 
+$invalididsum
