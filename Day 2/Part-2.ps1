a21f88328f0e1e9ff66c467800e2419adc5a8c05
C Fitzgerald <84927940+chloefitzgerald04@users.noreply.github.com>
2025-12-03 13:05:28 +0000

    Update Part-2.ps1
    slightly quicker

--- Day 2/Part-2.ps1
+++ Day 2/Part-2.ps1
@@ -1,14 +1,12 @@
-$ids = @((get-content -path "C:\Temp\data.csv" -delimiter ",").replace(",",""))
+$ids = @((get-content -path "C:\Temp\aoc2.csv" -delimiter ",").replace(",",""))
 $invalididsum = 0
 
 
 foreach($id in $ids){
     for($num = ([int64]$id.split("-")[0]); $num -le ([int64]$id.split("-")[1]); $num++){
-        $numstring = "$num"
         #Regex catches any repeating string
-        if ($numstring -match '^(.+)\1+$') {
-            $repeating = $matches[1]
-            $global:invalididsum += $num
+        if ($num.ToString() -match '^(.+)\1+$') {
+            $invalididsum += $num
         }
     }
 
